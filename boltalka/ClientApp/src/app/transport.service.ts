6dd44461baded2ba6b90c7de7970ca1280a27be0
D. Wolf <forecastdesigner@gmail.com>
2019-10-27 17:27:48 +0500

    Transport services in process

---
+++ boltalka/ClientApp/src/app/transport.service.ts
@@ -0,0 +1,44 @@
+import { Injectable } from '@angular/core';
+import * as signalR from "@aspnet/signalr";
+import { User } from "./user.model";
+import { IncomingMessage } from './incoming-message.model';
+
+const connection = new signalR.HubConnectionBuilder()
+    .withUrl("/hub")
+    .configureLogging(signalR.LogLevel.Information)
+    .build();
+
+@Injectable({
+  providedIn: 'root'
+})
+export class TransportService {
+    // online list
+    usersonline: Array<User>;
+    // offline list
+    usersoffline: Array<User>;
+    incomingmessage: Array<IncomingMessage>;
+    public connectionExists: Boolean;
+
+    constructor() { }
+
+    // Event handlers from the server
+    private registerOnServerEvents(): void {
+        let self: TransportService = this;
+
+        connection.on("onConnectedUsersOnlineList", (usersonline: Array<User>) => {
+            this.clearUserList();
+            usersonline.map(u => self.addUserInUsersList(u));
+        });
+    }
+
+    clearUserList = (): void => {
+        for (let n: number = this.usersonline.length; n > 0; n--) {
+            this.usersonline.pop();
+        }
+    }
+
+    addUserInUsersList = (useronline: User): void => {
+        this.usersonline.push(useronline);
+        //this.ChangeUsersCount.emit(this.usersonline.length);
+    }
+}
