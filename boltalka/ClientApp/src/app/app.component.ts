14962a154b2c258e8bdbc8f1cb9bf1c93c9d4590
D. Wolf <forecastdesigner@gmail.com>
2019-10-27 16:47:44 +0500

    Добавьте файлы проекта.

---
+++ boltalka/ClientApp/src/app/app.component.ts
@@ -0,0 +1,63 @@
+import { Component } from '@angular/core';
+import * as signalR from "@aspnet/signalr";
+
+const username = new Date().getTime();
+
+
+const connection = new signalR.HubConnectionBuilder()
+    .withUrl("/hub")
+    .configureLogging(signalR.LogLevel.Information)
+    .build();
+
+
+@Component({
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+
+    private connection: any;
+
+    title = 'ClientApp';
+    messages: Array<string> = [
+        'Dr Nice',
+        'Narco',
+        'Bombasto',
+        'Celeritas',
+        'Magneta',
+        'RubberMan',
+        'Dynama',
+        'Dr IQ',
+        'Magma',
+        'Tornado'
+    ];
+
+    sendmessage: string;
+
+
+    constructor() {
+
+        connection.on("messageReceived", (username: string, receivemessage: string) => {
+            this.messages.push(username + ': ' + receivemessage);
+        });
+
+        connection.start().catch(function (err) {
+            return console.error(err.toString());
+        });
+
+
+    }
+    //Receive message
+
+
+    //Отправка сообщения
+    onClickSendMsg = function (): void {
+        connection.send("newMessage", username, this.sendmessage)
+            .then(() => this.sendmessage = '');
+    };
+
+    private registerOnServerEvents(): void {
+
+    }
+}
---
+++ boltalka/Hubs/ChatHub.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.AspNetCore.SignalR;
+using System.Threading.Tasks;
+
+namespace boltalka.Hubs
+{
+    public class ChatHub : Hub
+    {
+        public async Task NewMessage(long username, string message)
+        {
+            await Clients.All.SendAsync("messageReceived", username, message);
+        }
+    }
+}
